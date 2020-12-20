208e3efd701b78d957943f2b9f9462620b446825
Mark Engelmann <61390579+vinegar-tom@users.noreply.github.com>
2020-12-19 19:29:53 -0600

    Fix day 15-2

--- day15/2.js
+++ day15/2.js
@@ -1,19 +1,31 @@
 const numberSpoken = '15,5,1,4,7,0'.split(',');
 
 const turnsSpoken = new Map();
+let previousNumber;
+
 numberSpoken.forEach((number, index) => {
-  turnsSpoken.set(Number(number), index);
+  turnsSpoken.set(Number(number), [index]);
+  if (index === numberSpoken.length - 1) previousNumber = Number(number);
 })
 
-let previousNumber =
+for (let i = turnsSpoken.size; i <= 29999999; i++) {
+  const prevNumTurns = turnsSpoken.get(previousNumber);
 
-for (let i = numberSpoken.length; i <= 29999999; i++) {
-  const previousNumber = numberSpoken[i - 1];
-  const lastIndex = numberSpoken.lastIndexOf(previousNumber);
-  const secondIndex = numberSpoken.lastIndexOf(previousNumber, lastIndex - 1);
+  if (prevNumTurns.length === 1) {
+    turnsSpoken.get(0).push(i);
+    previousNumber = 0;
+  } else {
+    const difference = prevNumTurns[prevNumTurns.length - 1] -
+      prevNumTurns[prevNumTurns.length - 2];
 
-  if (secondIndex === -1) numberSpoken.push(0);
-  else numberSpoken.push(lastIndex - secondIndex);
+    if (turnsSpoken.has(difference)) {
+      turnsSpoken.get(difference).push(i);
+    } else {
+      turnsSpoken.set(difference, [i]);
+    }
+
+    previousNumber = difference;
+  }
 }
 
-console.log(turnsSpoken);
+console.log(previousNumber);
